3da91b110289a4149ef4cf45b2f291199ec70d3f
Ubuntu <ubuntu@ip-172-31-85-225.ec2.internal>
2019-08-06 22:00:30 +0000

    add Procfile

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['337dc31fe4494c90adc076dcd14158d2.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = ['337dc31fe4494c90adc076dcd14158d2.vfs.cloud9.us-east-1.amazonaws.com',  'https://ekomerzz.herokuapp.com']
 
 
 # Application definition
